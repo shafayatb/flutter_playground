d5516eef813102cb6a7315030ffe411932551ead
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-01-21 13:46:45 +0600

    Stateless and Stateful Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() async {
-  runApp(
-      MaterialApp(
-          home: Scaffold(
-            body: Center(child: Text("Hello Mother"))
-          )
-      ));
+  runApp(MainWidget());
 }
+
+class MainWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+          home: Scaffold(
+              body: Center(child: CustomTextWidget())
+          )
+      );
+  }
+}
+
+class CustomTextWidget extends StatefulWidget {
+  const CustomTextWidget({super.key});
+
+  @override
+  State<CustomTextWidget> createState() => _CustomTextWidgetState();
+}
+
+class _CustomTextWidgetState extends State<CustomTextWidget> {
+
+  String text = 'Hello Mom';
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 40, color: Colors.amber),
+      ),
+      onTap: (){
+        print("Tapped");
+        setState(() {
+          text = 'Hello Bro';
+        });
+      },
+    );
+  }
+}
+
