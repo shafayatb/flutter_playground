becafd35fb3a6beab87aa0c74e798375860333f7
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-01-22 12:57:45 +0600

    Image Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,42 @@ class MainWidget extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(width: 200, height: 200, color: Colors.green),
-              Container(width: 100, height: 100, color: Colors.red),
-              Positioned(
-                bottom: 40,
-                right: 40,
-                child: Container(width: 20, height: 20, color: Colors.yellow),
-              ),
-            ],
+          child: Image.network(
+            "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp",
           ),
+          /*Image.asset(
+            'assets/axe 1.png',
+            width: 200,
+            height: 200,
+            fit: BoxFit.contain,
+          ),*/
         ),
       ),
+    );
+  }
+}
+
+class CustomStackWidget extends StatefulWidget {
+  const CustomStackWidget({super.key});
+
+  @override
+  State<CustomStackWidget> createState() => _CustomStackWidgetState();
+}
+
+class _CustomStackWidgetState extends State<CustomStackWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(width: 200, height: 200, color: Colors.green),
+        Container(width: 100, height: 100, color: Colors.red),
+        Positioned(
+          bottom: 40,
+          right: 40,
+          child: Container(width: 20, height: 20, color: Colors.yellow),
+        ),
+      ],
     );
   }
 }
