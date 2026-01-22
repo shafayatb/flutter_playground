93bef757443d9402a7832e382489a8fcac3028a0
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-01-22 12:49:37 +0600

    Stack widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,23 @@ class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-          home: Scaffold(
-              body: Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 200,
-                      color: Colors.green,
-                    ),
-                    Positioned(
-                      top: 20,
-                      right: 20,
-                      left: 20,
-                      bottom: 20,
-                      child: Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-          )
-      );
+      home: Scaffold(
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(width: 200, height: 200, color: Colors.green),
+              Container(width: 100, height: 100, color: Colors.red),
+              Positioned(
+                bottom: 40,
+                right: 40,
+                child: Container(width: 20, height: 20, color: Colors.yellow),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -54,28 +46,15 @@ class _RowColumnWidgetState extends State<RowColumnWidget> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 100,
-              height: 150,
-              color: Colors.red,
-            ),
-            Container(
-              width: 100,
-              height: 150,
-              color: Colors.blueAccent,
-            )
+            Container(width: 100, height: 150, color: Colors.red),
+            Container(width: 100, height: 150, color: Colors.blueAccent),
           ],
         ),
-        Container(
-          width: 200,
-          height: 150,
-          color: Colors.greenAccent,
-        )
+        Container(width: 200, height: 150, color: Colors.greenAccent),
       ],
     );
   }
 }
-
 
 class CustomTextWidget extends StatefulWidget {
   const CustomTextWidget({super.key});
@@ -85,7 +64,6 @@ class CustomTextWidget extends StatefulWidget {
 }
 
 class _CustomTextWidgetState extends State<CustomTextWidget> {
-
   String text = 'Hello Mom';
 
   @override
@@ -96,16 +74,13 @@ class _CustomTextWidgetState extends State<CustomTextWidget> {
         height: 300,
         decoration: BoxDecoration(
           color: Colors.blueAccent,
-          shape: BoxShape.circle
+          shape: BoxShape.circle,
         ),
         alignment: Alignment.center,
         padding: EdgeInsets.all(20),
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 40, color: Colors.amber),
-        ),
+        child: Text(text, style: TextStyle(fontSize: 40, color: Colors.amber)),
       ),
-      onTap: (){
+      onTap: () {
         print("Tapped");
         setState(() {
           text = 'Hello Bro';
@@ -114,4 +89,3 @@ class _CustomTextWidgetState extends State<CustomTextWidget> {
     );
   }
 }
-
