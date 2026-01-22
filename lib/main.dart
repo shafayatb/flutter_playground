f6943f7498a8ac5188bbb2bdf25f7c7981b01152
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-01-22 12:46:45 +0600

    Positioned Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,71 @@ class MainWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
           home: Scaffold(
-              body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
+              body: Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 200,
+                      color: Colors.green,
+                    ),
+                    Positioned(
+                      top: 20,
+                      right: 20,
+                      left: 20,
+                      bottom: 20,
+                      child: Container(
                         width: 100,
-                        height: 150,
+                        height: 100,
                         color: Colors.red,
                       ),
-                      Container(
-                        width: 100,
-                        height: 150,
-                        color: Colors.blueAccent,
-                      )
-                    ],
-                  ),
-                  Container(
-                    width: 200,
-                    height: 150,
-                    color: Colors.greenAccent,
-                  )
-                ],
+                    ),
+                  ],
+                ),
               )
           )
       );
   }
 }
+
+class RowColumnWidget extends StatefulWidget {
+  const RowColumnWidget({super.key});
+
+  @override
+  State<RowColumnWidget> createState() => _RowColumnWidgetState();
+}
+
+class _RowColumnWidgetState extends State<RowColumnWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 150,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 150,
+              color: Colors.blueAccent,
+            )
+          ],
+        ),
+        Container(
+          width: 200,
+          height: 150,
+          color: Colors.greenAccent,
+        )
+      ],
+    );
+  }
+}
+
 
 class CustomTextWidget extends StatefulWidget {
   const CustomTextWidget({super.key});
