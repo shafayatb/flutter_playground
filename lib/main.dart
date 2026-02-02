922a93155e888392369447d22689f63347b4e83a
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-02-02 11:29:00 +0600

    Custom button widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,27 +18,7 @@ class _MainWidgetState extends State<MainWidget> {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              print("Oh that hit the spot");
-            },
-            child: Text("Press Me"),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFFDFB25D),
-              foregroundColor: Color(0xFFB50853),
-              padding: EdgeInsets.symmetric(
-                horizontal: 40,
-                vertical: 10
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  width: 1,
-                  color: Colors.deepOrange
-                )
-              )
-            ),
-          ),
+          child: CustomButton(),
         ),
       ),
     );
@@ -56,6 +36,45 @@ width: 200,
 height: 200,
 fit: BoxFit.contain,
 ),*/
+
+class CustomButton extends StatefulWidget {
+  const CustomButton({super.key});
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  String buttonText = 'Press Me';
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        print("Oh that hit the spot");
+        setState(() {
+          buttonText = 'Oh Yaas';
+        });
+      },
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xFFDFB25D),
+          foregroundColor: Color(0xFFB50853),
+          padding: EdgeInsets.symmetric(
+              horizontal: 40,
+              vertical: 10
+          ),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+              side: BorderSide(
+                  width: 1,
+                  color: Colors.deepOrange
+              )
+          )
+      ),
+      child: Text(buttonText),
+    );
+  }
+}
+
 
 class CustomEditText extends StatefulWidget {
   const CustomEditText({super.key});
