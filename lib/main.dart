68f214ec0465e0af85c66649943e4ddcd897deb2
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-02-05 12:29:13 +0600

    List generate

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,15 @@ class CustomWidgetList extends StatefulWidget {
 class _CustomWidgetListState extends State<CustomWidgetList> {
   @override
   Widget build(BuildContext context) {
-    List<Widget> widgets = [
+    List<Widget> widgets = List.generate(10, (int index) {
+      return Container(
+        padding: EdgeInsets.all(5),
+        color: Colors.lightBlue,
+        child: Text('Index: ${index + 1}'),
+      );
+    });
+    /*
+    [
       Container(
         width: 100,
         height: 100,
@@ -71,7 +79,7 @@ class _CustomWidgetListState extends State<CustomWidgetList> {
         height: 130,
         color: Colors.blue,
       ),
-    ];
+    ];*/
     return Column(
       children: [
         Container(
@@ -84,17 +92,7 @@ class _CustomWidgetListState extends State<CustomWidgetList> {
           height: 130,
           color: Colors.blue,
         ),
-        ...widgets,
-        Container(
-          width: 80,
-          height: 70,
-          color: Colors.deepOrangeAccent,
-        ),
-        Container(
-          width: 120,
-          height: 130,
-          color: Colors.blue,
-        ),
+        ...widgets
       ]
     );
   }
