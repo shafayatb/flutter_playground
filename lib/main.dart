495f7e019efdd09b4d68c79675cbc02303e82941
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-02-08 13:02:48 +0600

    Bottom Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -163,29 +163,42 @@ class ProfilePage extends StatelessWidget {
       child: Center(
         child: ElevatedButton(
           onPressed: () {
-            showModalBottomSheet(
+            showBottomSheet(
               context: context,
               builder: (BuildContext context) {
                 return Container(
-                  height: 250,
-                  decoration: BoxDecoration(
+                  height: 500,
+                  /*decoration: BoxDecoration(
                     color: Colors.greenAccent,
                     borderRadius: BorderRadius.vertical(
                       top: Radius.circular(20),
                     ),
-                  ),
+                  ),*/
                   child: Center(
-                    child: Text(
-                      'Change Name',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                        fontSize: 30,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        'Change Name',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                          fontSize: 30,
+                        ),
                       ),
                     ),
                   ),
                 );
               },
+              enableDrag: false,
+              showDragHandle: true,
+              backgroundColor: Colors.black38,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(8)
+                )
+              )
             );
           },
           child: const Text('More Profile Options'),
