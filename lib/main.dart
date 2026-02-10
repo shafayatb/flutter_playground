0defae32e08be24c805d40613eb8705dd605f69d
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-02-10 13:01:21 +0600

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  String? username = await getUsername();
-  print(username);
+  await Firebase.initializeApp(
+
+    options: DefaultFirebaseOptions.currentPlatform,
+
+  );
 }
 
 Future<void> saveUsername(String username) async {
