f5b77ae027542487d1788667e8d0d2de8123a681
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2026-02-10 12:14:47 +0600

    Animated Container

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,48 @@ class AnimationExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Center(child: AnimationContainer())),
+      home: Scaffold(body: Center(child: CustomAnimatedContainer())),
+    );
+  }
+}
+
+class CustomAnimatedContainer extends StatefulWidget {
+  const CustomAnimatedContainer({super.key});
+
+  @override
+  State<CustomAnimatedContainer> createState() =>
+      _CustomAnimatedContainerState();
+}
+
+class _CustomAnimatedContainerState extends State<CustomAnimatedContainer> {
+  double size = 100;
+  Color color = Colors.lightBlueAccent;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          if (size >= 200) {
+            size = 100;
+          } else {
+            size += 50;
+          }
+
+          if (color == Colors.black38) {
+            color = Colors.lightBlueAccent;
+          } else {
+            color = Colors.black38;
+          }
+        });
+      },
+      child: AnimatedContainer(
+        height: size,
+        width: size,
+        color: color,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.elasticInOut,
+      ),
     );
   }
 }
